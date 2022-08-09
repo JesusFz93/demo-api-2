7566263097bc69e8ffc8b94a252c07bfd515819a
Jesus <jesus.fdz93@gmail.com>
2022-08-08 20:34:32 -0500

    post

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const app = express();
 const PORT = 4003;
+const { v4: uuid } = require("uuid");
 
 const { users } = require("./database/db");
 
@@ -14,6 +15,24 @@ app.get("/", (req, res) => {
     ok: true,
     msg: "Usuarios obtenidos",
     data: users,
+  });
+});
+
+app.post("/", (req, res) => {
+  const { user_name, password } = req.body;
+
+  const user = {
+    id: uuid(),
+    user_name: user_name,
+    password: password,
+  };
+
+  users.push(user);
+
+  return res.json({
+    ok: true,
+    msg: "Usuario creado",
+    data: user,
   });
 });
 
