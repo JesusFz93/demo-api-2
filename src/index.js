f06a4678d0149e52cc12bcf1f8e6d817c1bb0d87
Jesus <jesus.fdz93@gmail.com>
2022-08-10 21:03:35 -0500

    variables de entorno

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 // 1. IMPORTACIONES
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = 4003;
+const PORT = process.env.PUERTO;
 
 // 2.MIDDLEWARES
 app.use(express.json());
 
 // 3.ROUTES
+app.get("/", (req, res) => {
+  return res.json({
+    msg: "Bienvenido al API",
+    autor: process.env.AUTOR,
+  });
+});
 app.use("/api/users", require("./routes/users.routes"));
 
 // 4. SERVIDOR
