04b43088ec9771c7e9d6a8b381885d1b4436d47e
Jesus <jesus.fdz93@gmail.com>
2022-08-23 11:45:11 -0500

    get

--- src/database/config.js
+++ src/database/config.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const dbConnection = () => {
   try {
-    mongoose.connect(process.env.MONGODB_CONNECTION);
+    mongoose.connect(process.env.MONGODB_CONNECTION, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     console.log("Base de datos conectada");
   } catch {
     console.log("Error en la base de datos");
