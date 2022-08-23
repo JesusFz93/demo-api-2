7366f1ccd7846340b9ce1155b3b25b4201f7ea11
Jesus <jesus.fdz93@gmail.com>
2022-08-23 11:49:19 -0500

    opciones mongoose

--- src/database/config.js
+++ src/database/config.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dbConnection = () => {
   try {
     // https://arunrajeevan.medium.com/understanding-mongoose-connection-options-2b6e73d96de1 - para mas info acerca de opciones adicionales de mongoose
+
     mongoose.connect(process.env.MONGODB_CONNECTION, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
