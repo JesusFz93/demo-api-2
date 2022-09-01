70ef96028d6cd5a1004d218a2678630531a10aab
Jesus <jesus.fdz93@gmail.com>
2022-08-31 21:28:48 -0500

    middleware

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -7,9 +7,11 @@ const {
   eliminarProducto,
 } = require("../controllers/products.controller");
 
+const { validarJWT } = require("../middlewares/jwt.middleware");
+
 const router = Router();
 
-router.get("/", obtenerProductos);
+router.get("/", validarJWT, obtenerProductos);
 router.post("/", crearProducto);
 router.put("/:idProducto", actualizarProducto);
 router.delete("/:idProducto", eliminarProducto);
