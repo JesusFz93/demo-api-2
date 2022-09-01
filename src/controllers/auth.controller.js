eaa3afe3fa007ff9d2b4f5571db3eaec75a2e30e
Jesus <jesus.fdz93@gmail.com>
2022-08-31 20:09:18 -0500

    token listo

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,42 +1,54 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+const { generarJWT } = require("../helpers/jwt.helper");
 
 const registerUser = async (req, res) => {
-  const { email, username, password } = req.body;
+  try {
+    const { email, username, password } = req.body;
 
-  const emailEncontrado = await User.findOne({ email: email });
+    const emailEncontrado = await User.findOne({ email: email });
 
-  if (emailEncontrado) {
-    return res.status(400).json({
+    if (emailEncontrado) {
+      return res.status(400).json({
+        ok: false,
+        msg: `Error, el correo ${emailEncontrado.email} ya esta registrado`,
+      });
+    }
+
+    const usernameEncontrado = await User.findOne({ username: username });
+
+    if (usernameEncontrado) {
+      return res.status(400).json({
+        ok: false,
+        msg: `Error, el username ${usernameEncontrado.username} ya esta registrado`,
+      });
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+
+    const user = {
+      email: email,
+      username: username,
+      password: bcrypt.hashSync(password, salt),
+    };
+
+    const nuevoUsuario = await User.create(user);
+
+    const token = await generarJWT(nuevoUsuario.id);
+
+    return res.status(201).json({
+      ok: true,
+      msg: "Registro exitoso",
+      data: nuevoUsuario,
+      token,
+    });
+  } catch (error) {
+    return res.status(500).json({
       ok: false,
-      msg: `Error, el correo ${emailEncontrado.email} ya esta registrado`,
+      msg: "Problemas del lado del servidor",
+      data: [],
     });
   }
-
-  const usernameEncontrado = await User.findOne({ username: username });
-
-  if (usernameEncontrado) {
-    return res.status(400).json({
-      ok: false,
-      msg: `Error, el username ${usernameEncontrado.username} ya esta registrado`,
-    });
-  }
-
-  const salt = bcrypt.genSaltSync(10);
-
-  const user = {
-    email: email,
-    username: username,
-    password: bcrypt.hashSync(password, salt),
-  };
-
-  const nuevoUsuario = await User.create(user);
-
-  return res.status(201).json({
-    ok: true,
-    msg: "Registro exitoso",
-    data: nuevoUsuario,
-  });
 };
 
 module.exports = {
